e86c4b5b5967139c8d676fd628c13cb308e012ce
Vivian H <vhuang0102@gmail.com>
2020-08-17 04:24:28 +0000

    Initial commit

---
+++ quiz.js
@@ -0,0 +1,39 @@
+function q1a1() {
+  document.getElementById("madePerYear").innerHTML = "Wrong. It's 150 billion garments made per year. This is enough to give everyone on the planet 20 pieces of new clothing per year!";
+}
+function q1a2() {
+  document.getElementById("madePerYear").innerHTML = "Wrong. It's 150 billion garments made per year. This is enough to give everyone on the planet 20 pieces of new clothing per year!";
+}
+function q1a4() {
+  document.getElementById("madePerYear").innerHTML = "Wrong. It's 150 billion garments made per year. This is enough to give everyone on the planet 20 pieces of new clothing per year!";
+}
+function q1a3() {
+  document.getElementById("madePerYear").innerHTML = "Right! It's 150 billion garments made per year. This is enough to give everyone on the planet 20 pieces of new clothing per year!";
+}
+
+function q2a1() {
+  document.getElementById("pollution").innerHTML = "Nope. It's the third highest polluting industry in the world and it is the second largest consumer of water! 2000 different chemicals are used in making clothes.";
+}
+function q2a2() {
+  document.getElementById("pollution").innerHTML = "Yep! It's the third highest polluting industry in the world and it is the second largest consumer of water! 2000 different chemicals are used in making clothes.";
+}
+function q2a3() {
+  document.getElementById("pollution").innerHTML = "Nope. It's the third highest polluting industry in the world and it is the second largest consumer of water! 2000 different chemicals are used in making clothes.";
+}
+function q2a4() {
+  document.getElementById("pollution").innerHTML = "Nope. It's the third highest polluting industry in the world and it is the second largest consumer of water! 2000 different chemicals are used in making clothes.";
+}
+
+function q3a1() {
+  document.getElementById("trash").innerHTML = "Correct. This is because of fast fashion, where clothes are made cheaply and are intended for you to keep consuming clothes to keep up with trends, which is not only detrimental to the environment, but also causes workers to be exploited.";
+}
+function q3a2() {
+  document.getElementById("trash").innerHTML = "Incorrect. On average, clothes are just worn seven times. This is because of fast fashion, where clothes are made cheaply and are intended for you to keep consuming clothes to keep up with trends, which is not only detrimental to the environment, but also causes workers to be exploited.";
+}
+function q3a3() {
+  document.getElementById("trash").innerHTML = "Incorrect. On average, clothes are just worn seven times. This is because of fast fashion, where clothes are made cheaply and are intended for you to keep consuming clothes to keep up with trends, which is not only detrimental to the environment, but also causes workers to be exploited.";
+}
+function q3a4() {
+  document.getElementById("trash").innerHTML = "Incorrect. On average, clothes are just worn seven times. This is because of fast fashion, where clothes are made cheaply and are intended for you to keep consuming clothes to keep up with trends, which is not only detrimental to the environment, but also causes workers to be exploited.";
+}
+
